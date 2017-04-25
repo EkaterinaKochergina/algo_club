80b335fd0ded7000e6079df3533d0c6a777ce1d3
ekaterinakochergina <kochergina_kat@mail.ru>
2017-04-26 00:34:27 +0300

    matrix

--- onpython/sol_kat/approx/matrix.py
+++ onpython/sol_kat/approx/matrix.py
@@ -1,5 +1,4 @@
 import unittest
-
 class Matrix(object):
     def __init__(self, dat):
         if type(dat)==list and type(dat[0])==list:
@@ -16,12 +15,42 @@ class Matrix(object):
     def get(self, p, q):
         return self.dat[p][q]
 
+    def __repr__(self):
+        return 'Matrix(%s)'%self.dat
+    
+    def __mul__(self, r):
+        res=Matrix(len(self.dat), len(r.dat[0]))
+        for p in range(len(self.dat)):        
+            for q in range(len(r.dat[0])):
+                val=0
+                for ind in range(len(self.dat[0])):
+                    val +=self.dat[p][ind]*r.dat[ind][q]
+                res.dat[p][q]=val
+        return res
+
+    def set(self, p, q, val):
+        print('set: p=', p, 'q=', q, 'val=', val, 'dat=', self.dat)
+        self.dat[p][q] = val
+        
+    def transpose(self):
+        h, w=len(self.dat), len(self.dat[0])
+        tm=Matrix(w, h)
+        for p in range(h):
+            for q in range(w):
+                tm.dat[q][p]=self.dat[p][q]
+        return tm
+
+    def copy(mat):
+        return[t.copy() for t in self.dat]
+
 class TestMatrix(unittest.TestCase):
     def test_matrix_init(self):
         mat = Matrix([[11,12],[21,22],[31,32]])
         self.assertEqual(mat.height(), 3)
         self.assertEqual(mat.width(), 2)
         self.assertEqual(mat.get(2, 1), 32)
+        self.assertEqual(mat.set(2, 1), 32)
+        self.assertEqual(mat.transpose(), ([11, 21, 31], [12, 22, 32])
 
 if __name__ == '__main__':
     unittest.main()
