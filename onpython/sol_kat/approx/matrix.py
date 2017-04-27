3e2e7f808427ef357141e3ca4d55e29038477078
ekaterinakochergina <kochergina_kat@mail.ru>
2017-04-27 21:28:39 +0300

    new unittests

--- onpython/sol_kat/approx/matrix.py
+++ onpython/sol_kat/approx/matrix.py
@@ -1,13 +1,27 @@
+import copy
 import unittest
+
+
 class Matrix(object):
     def __init__(self, dat):
-        if type(dat)==list and type(dat[0])==list:
-            self.dat=dat
-        elif type(dat) == list:
-            self.dat= [[t] for t in dat]
+        if isinstance(dat, Matrix):
+            self.dat = copy.deepcopy(dat.dat)
+        else:
+            self.dat = copy.deepcopy(dat)
+
+    @classmethod
+    def vector_init(cls, vec):
+        return cls([[t] for t in vec])
+
+    @classmethod
+    def shape_init(cls, h, w):
+        return cls([[0]*w for _ in range(h)])
+
+    def __repr__(self):
+        return "Matrix({!r})".format(self.dat)
 
     def width(self):
-                return len(self.dat[0])
+        return len(self.dat[0])
  
     def height(self):
         return len(self.dat)
@@ -19,29 +33,32 @@ class Matrix(object):
         return 'Matrix(%s)'%self.dat
     
     def __mul__(self, r):
-        res=Matrix(len(self.dat), len(r.dat[0]))
+        res = Matrix.shape_init(len(self.dat), len(r.dat[0]))
         for p in range(len(self.dat)):        
             for q in range(len(r.dat[0])):
                 val=0
                 for ind in range(len(self.dat[0])):
-                    val +=self.dat[p][ind]*r.dat[ind][q]
+                    val += self.dat[p][ind] * r.dat[ind][q]
                 res.dat[p][q]=val
         return res
 
-    def set(self, p, q, val):
-        print('set: p=', p, 'q=', q, 'val=', val, 'dat=', self.dat)
-        self.dat[p][q] = val
-        
-    def transpose(self):
-        h, w=len(self.dat), len(self.dat[0])
-        tm=Matrix(w, h)
-        for p in range(h):
-            for q in range(w):
-                tm.dat[q][p]=self.dat[p][q]
-        return tm
+    def __eq__(self, other):
+        return self.dat == other.dat
 
-    def copy(mat):
-        return[t.copy() for t in self.dat]
+#    def set(self, p, q, val):
+#        print('set: p=', p, 'q=', q, 'val=', val, 'dat=', self.dat)
+#        self.dat[p][q] = val
+        
+#    def transpose(self):
+#        h, w=len(self.dat), len(self.dat[0])
+#        tm=Matrix(w, h)
+#        for p in range(h):
+#            for q in range(w):
+#                tm.dat[q][p]=self.dat[p][q]
+#        return tm
+
+#    def copy(mat):
+#        return[t.copy() for t in self.dat]
 
 class TestMatrix(unittest.TestCase):
     def test_matrix_init(self):
@@ -49,8 +66,26 @@ class TestMatrix(unittest.TestCase):
         self.assertEqual(mat.height(), 3)
         self.assertEqual(mat.width(), 2)
         self.assertEqual(mat.get(2, 1), 32)
-        self.assertEqual(mat.set(2, 1), 32)
-        self.assertEqual(mat.transpose(), ([11, 21, 31], [12, 22, 32])
+#        self.assertEqual(mat.set(2, 1), 32)
+#        self.assertEqual(mat.transpose(), ([11, 21, 31], [12, 22, 32]))
+                         
+    def test_vector_init(self):
+        lst = [1, 2, 10, 15]
+        mat = Matrix.vector_init(lst)
+        self.assertEqual(mat.height(), len(lst))
+        self.assertEqual(mat.width(), 1)
+        self.assertEqual(mat.get(2, 0), 10)
+     
+    def test_shape(self):
+        mat = Matrix.shape_init(3, 5)
+        self.assertEqual(mat.height(), 3)
+        self.assertEqual(mat.width(), 5)
+
+    def test_mat_mul(self):
+        a = Matrix([[1, 2], [3, 5]])
+        b = Matrix([[-1, 1], [2, 0]])
+        c = a * b
+        self.assertEqual(c, Matrix([[3, 1], [7, 3]]))
 
 if __name__ == '__main__':
     unittest.main()
