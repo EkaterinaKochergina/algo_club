6ad6f257784bd095b1f99c8277a8c381f6b821bb
ekaterinakochergina <kochergina_kat@mail.ru>
2017-05-25 21:06:14 +0300

    комплекс

--- onpython/sol_kat/approx/unittest_demo/cmplx.py
+++ onpython/sol_kat/approx/unittest_demo/cmplx.py
@@ -2,19 +2,47 @@
                                                                                
 import unittest                                                                
 
-class Complex(object):                                                         
-    def __init__(self, a, b):                                                  
-        self.a = a                                           
-        self.b = b                                   
-                                                                               
-#    def __add__(self, obj):                                                   
-                                                                               
+class ComplexCartesian(object):                                                         
+    def __init__(self, real, imag):                                                  
+        self.real = real                                           
+        self.imag = imag                                   
+                     
+    def __repr__(self):
+        return 'ComplexCartesian({}, {})' .format(self.real, self.imag)
+                                         
+              
+    def __add__(self, other):
+        return ComplexCartesian(self.real + other.real, self.imag + other.imag)                                                   
+
+    # def __sub__(self, other):
+    #     return Complex(self.real + other.real, self.imag + other.imag)
+    
+    # def __div__(self, other):
+    #     return Complex(self.real - other.real, self.other - self.other)
+
+    
+class ComplexPolar(object):
+    pass
                                                                                
 class UnittestComplex(unittest.TestCase):                                        
     def test_Complex_create(self):                                             
-        a = Complex(2,3)                                                      
-        b = Complex(-4,1)                                                      
-                                                                               
+        num = ComplexCartesian(2.73, -12.14)                                              
+        self.assertEqual(num.real, 2.73)
+        self.assertEqual(num.imag, -12.14)
+
+    def test_repr_Complex_display(self):
+        num = ComplexCartesian(2.73, -12.14)  
+        self.assertEqual(repr(num), 'ComplexCartesian(2.73, -12.14)')  
+
+    def test_add_ComplexCartesian(self):
+        a = ComplexCartesian(1, 3)
+        b = ComplexCartesian(4, 7)
+        c = a + b
+        self.assertAlmostEqual(c.real, 5)
+        self.assertAlmostEqual(c.imag, 10)
+
+
+            
 #    def test_sum_complex(self):                                               
 #        self.assertEqual(a + b, Complex())                                    
                                                                                
